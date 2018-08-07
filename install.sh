31e99df88835993e668436d1f00406815456b632
Michael Zhao <mzhaox+github@gmail.com>
2018-08-06 22:08:05 -0600

    Avoid specifying versions on Haskell things

--- install.sh
+++ install.sh
@@ -73,7 +73,7 @@ sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:hvr/ghc
 sudo apt-get update
-sudo apt-get install -y cabal-install-1.22 ghc-7.10.3
+sudo apt-get install -y cabal-install ghc
 cat >> ~/.zshrc <<EOF
 export PATH="\$HOME/.cabal/bin:/opt/cabal/1.22/bin:/opt/ghc/7.10.3/bin:\$PATH"
 EOF
